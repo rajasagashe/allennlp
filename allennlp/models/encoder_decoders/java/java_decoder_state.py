0e61f23e129c093c70fa96e70b1f648bddc7672d
rajas <rajasagashe@users.noreply.github.com>
2018-04-02 17:08:53 -0700

    boilerplate java decoder state

---
+++ allennlp/models/encoder_decoders/java/java_decoder_state.py
@@ -0,0 +1,40 @@
+from typing import Dict, List, Tuple
+
+import torch
+
+from allennlp.data.fields.production_rule_field import ProductionRuleArray
+from allennlp.modules import TextFieldEmbedder
+from allennlp.nn.decoding import DecoderState, RnnState
+from allennlp.semparse.type_declarations import JavaGrammarState
+
+
+class JavaDecoderState(DecoderState['JavaDecoderState']):
+    def __init__(self,
+                 action_history: List[int],
+                 score: torch.Tensor,
+                 rnn_state: RnnState,
+                 grammar_state: JavaGrammarState,
+                 nonterminal_embedder: TextFieldEmbedder,
+                 nonterminal: torch.Tensor,
+                 prev_rule: torch.Tensor,
+                 nonterminal2parent_rule: Dict[torch.LongTensor, torch.LongTensor],
+                 nonterminal2parent_state: Dict[torch.LongTensor, torch.LongTensor]) -> None:
+        super(JavaDecoderState, self).__init__(None, action_history, score)
+        self._grammar_state = grammar_state
+        self._rnn_state = rnn_state
+        if len(action_history) == 0:
+            # todo insert the method decl starting action in
+            print('todo')
+
+        self._nonterminal_embedder = nonterminal_embedder
+        self._nonterminal = nonterminal
+        self._prev_rule = prev_rule
+        self._nonterminal2parent_rule = nonterminal2parent_rule
+        self._nonterminal2parent_state = nonterminal2parent_state
+
+
+    def get_valid_actions(self) -> List[List[int]]:
+        """
+        Returns a list of valid actions for each element of the group.
+        """
+        return [state.get_valid_actions() for state in self.grammar_state]
